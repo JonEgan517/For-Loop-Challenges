551e0d6696851c61b4e1ec366d4349053475b30e
jonathan.egan <jonathan.egan@MU-C1-12.mullen.pvt.k12.co.us>
2019-09-04 13:01:04 -0600

    commit

---
+++ src/ForLoopChallenges.java
@@ -0,0 +1,60 @@
+
+public class ForLoopChallenges 
+{
+	public static void main(String[] args)
+	{
+		//iLoveDonuts();
+		ascendingDescending();
+		//multiples();
+		//totalOfMultiples();
+		//lowerBoundUpperBound();
+		//rectangleAsterisks();
+		//extraCredit();	
+	}
+
+	private static void iLoveDonuts() 
+	{
+		for(int i = 0; i < 5; i++)
+		{
+			System.out.println("I love Donuts");
+		}
+	}
+
+	private static void ascendingDescending() 
+	{
+		for(int i = 1; i <= 5; i++)
+		{
+			System.out.println(i);
+		}
+	}
+
+	private static void multiples() 
+	{
+		
+		
+	}
+
+	private static void totalOfMultiples() 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void lowerBoundUpperBound() 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void rectangleAsterisks() 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void extraCredit() 
+	{		
+		// TODO Auto-generated method stub
+		
+	}
+}
